01b6bf22692755765962edd0e9d31edc8ee06b2e
Maksym Soroka <ms.mikro.1@gmail.com>
2023-04-13 20:45:21 +0300

    v0.1.2

--- bus.go
+++ bus.go
@@ -8,19 +8,74 @@ import (
 )
 
 type bus struct {
-	conn           *amqp.Connection
-	chanel         *amqp.Channel
-	qName          string
-	replyQueueName string
-	senders        map[string]chan string
-	mutex          *sync.Mutex
+	chanel           *amqp.Channel
+	serviceQueueName string
+	replyQueueName   string
+	senders          map[string]chan string
+	mutex            *sync.Mutex
 }
 
-func NewBus(conn *amqp.Connection) *bus {
-	return &bus{
-		conn:  conn,
-		mutex: new(sync.Mutex),
+func NewBus(conn *amqp.Connection, queueName string) (*bus, error) {
+	chanel, err := conn.Channel()
+	if err != nil {
+		return nil, err
 	}
+
+	//serviceQueue, err := chanel.QueueDeclare(
+	_, err = chanel.QueueDeclare(
+		queueName, // name
+		false,     // durable
+		false,     // delete when unused
+		false,     // exclusive
+		false,     // no-wait
+		nil,       // arguments
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	replyToQueue, err := chanel.QueueDeclare(
+		"",    // name
+		false, // durable
+		true,  // delete when unused
+		true,  // exclusive
+		false, // no-wait
+		nil,   // arguments
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	replyToMsgs, err := chanel.Consume(
+		replyToQueue.Name, // queue
+		"",                // consumer
+		true,              // auto-ack
+		true,              // exclusive
+		false,             // no-local
+		false,             // no-wait
+		nil,               // args
+	)
+
+	b := &bus{
+		chanel:           chanel,
+		serviceQueueName: queueName,
+		replyQueueName:   replyToQueue.Name,
+		senders:          make(map[string]chan string),
+		mutex:            new(sync.Mutex),
+	}
+
+	go func() {
+		for m := range replyToMsgs {
+			b.mutex.Lock()
+			if c, ok := b.senders[m.CorrelationId]; ok {
+				c <- string(m.Body)
+				delete(b.senders, m.CorrelationId)
+			}
+			b.mutex.Unlock()
+		}
+	}()
+
+	return b, nil
 }
 
 func (b *bus) Send(ctx context.Context, msg []byte) ([]byte, error) {
